bcc589f09ffd7b157c220327a4635307b9d03f4a
Divine <officialkb17@gmail.com>
2024-09-25 16:31:39 +0100

    Write interactions script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,2 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedMoodNft);
+    }
+
+    function mintNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public constant TOKEN_ID_TO_FLIP = 0;
+
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        flipMoodNft(mostRecentlyDeployedMoodNft);
+    }
+
+    function flipMoodNft(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
+        vm.stopBroadcast();
+    }
+}
